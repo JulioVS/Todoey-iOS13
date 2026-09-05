e6878e4fcd893d3c536c040bfe790cee67b539df
JulioVS <julioer@me.com>
2026-09-05 13:09:05 -0300

    Add filter dismiss

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -144,4 +144,18 @@ extension TodoListViewController: UISearchBarDelegate {
                 
     }
     
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if searchBar.text?.count == 0 {
+
+            loadItems()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+            
+        }
+        
+    }
+    
 }
